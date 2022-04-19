f2071600b7ca6210f6d2f7ff73a67737ead6c0c7
Fan Jing <jing1999fan@outlook.com>
2022-04-19 03:23:18 -0700

    2st version

--- grabWeb/grabWeb.cs
+++ grabWeb/grabWeb.cs
@@ -25,42 +25,32 @@ namespace grabWeb
                     sw.Write(TranslatorLanguage (html));
 
                 }
-               
                 Console.ReadLine();
             }
         }
 
-        public static string TranslatorLanguage(string htmlStr)
+        public static  string TranslatorLanguage(string htmlStr)
         {
             string Language;
             bool exist=true;
 
-            /*string reg_style = "<style[^>]*[\\s\\S]*?<\\/style>";
-            string reg_script = "<script[^>]*[\\s\\S]*?<\\/script>";
-            string reg_html = "<[^>]+>";
-
-            htmlStr = Regex.Replace(htmlStr, reg_style,"");
-            htmlStr = Regex.Replace(htmlStr, reg_script, "");
-            //htmlStr = Regex.Replace(htmlStr, reg_html, "");
-            htmlStr = Regex.Replace(htmlStr, "\\s*|\t|\r|\n", "");
-            //htmlStr = htmlStr.Replace("", "");
-            return htmlStr.Trim();*/
-
-            string withoutNString = htmlStr.Replace("\n", " ");
-            string withoutRString = withoutNString.Replace("\r", " ");
-            string withoutTString = withoutRString.Replace("\t", " ");
-            string newString = withoutTString.Replace("\\", " ");
+            /*string withoutNString = htmlStr.Replace("\n", "");
+            string withoutRString = withoutNString.Replace("\r", "");
+            string withoutTString = withoutRString.Replace("\t", "");
+            string newString = withoutTString.Replace("\\", "    ");
 
             htmlStr  = Regex.Match(newString, @"<table.*>.*</table>").ToString();
-            //htmlStr  = Regex.Replace(htmlStr , @"<!--(?s).*?-->", "", RegexOptions.IgnoreCase);
-            //htmlStr  = Regex.Replace(htmlStr , @" ", "", RegexOptions.IgnoreCase);
             htmlStr = Regex.Replace(htmlStr, @"<div.*>.*</div>", "", RegexOptions.IgnoreCase);
             htmlStr = Regex.Replace(htmlStr, @"<p.*>.*</p>", "", RegexOptions.IgnoreCase);
             Regex reg = new Regex(@"<table.*?>[\s\S]*?<\/table>");
             MatchCollection match = reg.Matches(htmlStr);
             string newHtml = match[0].ToString();
-            return newHtml;
-            //return htmlStr.Trim();
+            return newHtml;*/
+
+            htmlStr = Regex.Match (htmlStr ,@"<table.*?>[\s\S]*?<\/table>").ToString();
+            htmlStr = Regex.Replace(htmlStr , "âœ”", "true");
+            htmlStr = Regex.Replace(htmlStr, "â€“", "-");
+            return htmlStr ;
         }
 
     }
